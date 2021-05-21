b7a6723bc1c1bedf575e3f00d619aa685bd9e2a6
raphael60650 <rc60650@hotmail.com>
2021-05-21 16:59:39 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,57 @@
+from cx_Freeze import setup, Executable
+import sys
+import glob
+
+include_files = [
+            "./icon.ico",
+            "./LICENSE",
+            "./README.md",
+            "./version",
+            "./convert_file.json",
+            "./fs.json",
+
+            "./tools",
+
+            "./file/Track-WU8",
+            "./file/video.thp",
+            "./file/Back.brctr",
+            "./file/cup_icon_64x64_common.brlyt",
+            "./file/CupSelectCup.brctr",
+            "./file/CourseSelectCup.brctr",
+            "./file/course_name.brlyt",
+            "./file/lecode-PAL.bin",
+            "./file/itemBoxNiseRtpa.brres",
+            "./file/RKRace.breff",
+            sys.exec_prefix + "\\DLLs\\tcl86t.dll",
+            sys.exec_prefix + "\\DLLs\\tk86t.dll",
+        ]
+
+include_files.extend(glob.glob("./file/*.txt"))
+include_files.extend(glob.glob("./file/*.png"))
+
+
+options = {
+    "build_exe":{
+        "include_files": include_files,
+        "includes": ["tkinter", "requests"],
+        "packages": [],
+        "excludes": []
+    }
+}
+
+setup(
+    options=options,
+    name='MKWF-Install',
+    version='0.1',
+    url='https://github.com/Faraphel/MKWF-Install',
+    license='Apache-2.0',
+    author='Faraphel',
+    author_email='rc60650@hotmail.com',
+    description='Installateur pour Mario Kart Wii Faraphel.',
+    executables = [Executable("./main.pyw",
+                              icon = "./icon.ico",
+                              base = "win32gui",
+                              target_name = "MKWF-Install.exe",
+                              shortcut_name = "MKWF-Install",
+                              shortcut_dir = "DesktopFolder")],
+)
