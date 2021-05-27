f4c753825a98ed41d1393c13da4bb2a3dfa42bad
raphael60650 <rc60650@hotmail.com>
2021-05-27 12:49:32 +0200

    4/3 image's background were in 16/9

--- source/patch_img_desc.py
+++ source/patch_img_desc.py
@@ -19,7 +19,7 @@ def patch_img_desc(self):
         new_16_9.paste(img_lang_16_9, (0, 0), img_lang_16_9)
         new_16_9.save(f"./file/strapA_16_9_832x456{get_filename(get_nodir(file_lang))}.png")
 
-        new_4_3 = Image.new("RGBA", (832, 456), (0, 0, 0, 255))
+        new_4_3 = Image.new("RGBA", (608, 456), (0, 0, 0, 255))
         new_4_3.paste(il_4_3, (0, 0), il_4_3)
         new_4_3.paste(img_lang_4_3, (0, 0), img_lang_4_3)
         new_4_3.save(f"./file/strapA_608x456{get_filename(get_nodir(file_lang))}.png")
