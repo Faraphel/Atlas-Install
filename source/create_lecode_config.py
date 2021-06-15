e4fa9ba95cf1b18b55d03cbf0e3d259924fca825
raphael60650 <rc60650@hotmail.com>
2021-06-15 17:03:31 +0200

    added try except in most function to help debugging error

--- source/create_lecode_config.py
+++ source/create_lecode_config.py
@@ -2,73 +2,77 @@ import json
 
 
 def create_lecode_config(self):
-    def get_star_text(track):
-        if "warning" in track: warning = "!" * track["warning"]
-        else: warning = ""
+    try:
+        def get_star_text(track):
+            if "warning" in track: warning = "!" * track["warning"]
+            else: warning = ""
 
-        if "score" in track:
-            if track["score"] > 0:
-                return "★" * track["score"] + "☆" * (3 - track["score"]) + warning + " "
-        return ""
+            if "score" in track:
+                if track["score"] > 0:
+                    return "★" * track["score"] + "☆" * (3 - track["score"]) + warning + " "
+            return ""
 
-    def get_ctfile_text(track, race=False):
-        track_name = track["name"].replace("_", "")
+        def get_ctfile_text(track, race=False):
+            track_name = track["name"].replace("_", "")
 
-        if "prefix" in track: prefix = f"{track['prefix']} "
-        else: prefix = ""
-        if "suffix" in track: suffix = f" ({track['suffix']})"
-        else: suffix = ""
+            if "prefix" in track: prefix = f"{track['prefix']} "
+            else: prefix = ""
+            if "suffix" in track: suffix = f" ({track['suffix']})"
+            else: suffix = ""
 
-        if race:
-            return f'  T {track["music"]}; ' + \
-                   f'{track["special"]}; ' + \
-                   f'{"0x01" if track["new"] else "0x00"}; ' + \
-                   f'"-"; ' + \
-                   f'"{get_star_text(track)}{prefix}{track_name}{suffix}\\n{track["author"]}"; ' + \
-                   f'"-"\n'
-        else:
-            return f'  T {track["music"]}; ' + \
-                   f'{track["special"]}; ' + \
-                   f'{"0x01" if track["new"] else "0x00"}; ' + \
-                   f'"{prefix}{track["name"]}{suffix}"; ' + \
-                   f'"{get_star_text(track)}{prefix}{track_name}{suffix}"; ' + \
-                   f'"-"\n'
+            if race:
+                return f'  T {track["music"]}; ' + \
+                       f'{track["special"]}; ' + \
+                       f'{"0x01" if track["new"] else "0x00"}; ' + \
+                       f'"-"; ' + \
+                       f'"{get_star_text(track)}{prefix}{track_name}{suffix}\\n{track["author"]}"; ' + \
+                       f'"-"\n'
+            else:
+                return f'  T {track["music"]}; ' + \
+                       f'{track["special"]}; ' + \
+                       f'{"0x01" if track["new"] else "0x00"}; ' + \
+                       f'"{prefix}{track["name"]}{suffix}"; ' + \
+                       f'"{get_star_text(track)}{prefix}{track_name}{suffix}"; ' + \
+                       f'"-"\n'
 
-    with open("./ct_config.json", encoding="utf-8") as f:
-        ctconfig = json.load(f)
+        with open("./ct_config.json", encoding="utf-8") as f:
+            ctconfig = json.load(f)
 
-    with open("./file/CTFILE.txt", "w", encoding="utf-8") as ctfile, \
-         open("./file/RCTFILE.txt", "w", encoding="utf-8") as rctfile:
+        with open("./file/CTFILE.txt", "w", encoding="utf-8") as ctfile, \
+             open("./file/RCTFILE.txt", "w", encoding="utf-8") as rctfile:
 
-        header = "#CT-CODE\n" +\
-                 "[RACING-TRACK-LIST]\n" +\
-                 "%LE-FLAGS=1\n" +\
-                 "%WIIMM-CUP=1\n" +\
-                 "N N$SWAP | N$F_WII\n\n"
-        ctfile.write(header)
-        rctfile.write(header)
+            header = "#CT-CODE\n" +\
+                     "[RACING-TRACK-LIST]\n" +\
+                     "%LE-FLAGS=1\n" +\
+                     "%WIIMM-CUP=1\n" +\
+                     "N N$SWAP | N$F_WII\n\n"
+            ctfile.write(header)
+            rctfile.write(header)
 
-        for cup in ctconfig["cup"]: # defined cup section
-            _cup_config = ctconfig["cup"][cup]
-            if int(cup) >= 9:  # Course qui ne sont ni les originales, ni les courses aléatoires.
-                cup = f'\nC "{_cup_config["name"]}"\n'
-                ctfile.write(cup)
-                rctfile.write(cup)
+            for cup in ctconfig["cup"]: # defined cup section
+                _cup_config = ctconfig["cup"][cup]
+                if int(cup) >= 9:  # Course qui ne sont ni les originales, ni les courses aléatoires.
+                    cup = f'\nC "{_cup_config["name"]}"\n'
+                    ctfile.write(cup)
+                    rctfile.write(cup)
 
-                for course in _cup_config["courses"]:
-                    _course_config = _cup_config["courses"][course]
-                    ctfile.write(get_ctfile_text(_course_config, race=False))
-                    rctfile.write(get_ctfile_text(_course_config, race=True))
+                    for course in _cup_config["courses"]:
+                        _course_config = _cup_config["courses"][course]
+                        ctfile.write(get_ctfile_text(_course_config, race=False))
+                        rctfile.write(get_ctfile_text(_course_config, race=True))
 
-        for i, _course_config in enumerate(ctconfig["tracks_list"]):  # undefined cup section
-            if i % 4 == 0:
-                cup = f'\nC "TL{i//4}"\n'
-                ctfile.write(cup)
-                rctfile.write(cup)
+            for i, _course_config in enumerate(ctconfig["tracks_list"]):  # undefined cup section
+                if i % 4 == 0:
+                    cup = f'\nC "TL{i//4}"\n'
+                    ctfile.write(cup)
+                    rctfile.write(cup)
 
-            ctfile.write(get_ctfile_text(_course_config, race=False))
-            rctfile.write(get_ctfile_text(_course_config, race=True))
+                ctfile.write(get_ctfile_text(_course_config, race=False))
+                rctfile.write(get_ctfile_text(_course_config, race=True))
 
-        for _ in range(1, 4-(i%4)):  # Complete cup if track are missing
-            ctfile.write(f'  T T44; T44; 0x00; "_"; ""; "-"\n')
-            rctfile.write(f'  T T44; T44; 0x00; "_"; ""; "-"\n')
+            for _ in range(1, 4-(i%4)):  # Complete cup if track are missing
+                ctfile.write(f'  T T44; T44; 0x00; "_"; ""; "-"\n')
+                rctfile.write(f'  T T44; T44; 0x00; "_"; ""; "-"\n')
+
+    except:
+        self.log_error()
--- source/install_mod.py
+++ source/install_mod.py
@@ -11,104 +11,109 @@ from .definition import *
 
 def install_mod(self):
     def func():
-        with open("./fs.json") as f:
-            fs = json.load(f)
+        try:
+            with open("./fs.json") as f:
+                fs = json.load(f)
 
-        # This part is used to estimate the max_step
-        extracted_file = []
-        max_step, step = 1, 0
+            # This part is used to estimate the max_step
+            extracted_file = []
+            max_step, step = 1, 0
 
-        def count_rf(path):
-            nonlocal max_step
-            max_step += 1
-            extension = get_extension(path)
-            if extension == "szs":
-                if not (os.path.realpath(path) in extracted_file):
-                    extracted_file.append(os.path.realpath(path))
-                    max_step += 1
+            def count_rf(path):
+                nonlocal max_step
+                max_step += 1
+                extension = get_extension(path)
+                if extension == "szs":
+                    if not (os.path.realpath(path) in extracted_file):
+                        extracted_file.append(os.path.realpath(path))
+                        max_step += 1
 
-        for fp in fs:
-            for f in glob.glob(self.path_mkwf + "/files/" + fp, recursive=True):
-                if type(fs[fp]) == str:
-                    count_rf(path=f)
-                elif type(fs[fp]) == dict:
-                    for nf in fs[fp]:
-                        if type(fs[fp][nf]) == str:
-                            count_rf(path=f)
-                        elif type(fs[fp][nf]) == list:
-                            for ffp in fs[fp][nf]: count_rf(path=f)
-        ###
-        extracted_file = []
-        max_step += 4  # PATCH main.dol et PATCH lecode.bin, conversion, changement d'ID
-        self.Progress(show=True, indeter=False, statut=self.translate("Installation du mod"), max=max_step, step=0)
+            for fp in fs:
+                for f in glob.glob(self.path_mkwf + "/files/" + fp, recursive=True):
+                    if type(fs[fp]) == str:
+                        count_rf(path=f)
+                    elif type(fs[fp]) == dict:
+                        for nf in fs[fp]:
+                            if type(fs[fp][nf]) == str:
+                                count_rf(path=f)
+                            elif type(fs[fp][nf]) == list:
+                                for ffp in fs[fp][nf]: count_rf(path=f)
+            ###
+            extracted_file = []
+            max_step += 4  # PATCH main.dol et PATCH lecode.bin, conversion, changement d'ID
+            self.Progress(show=True, indeter=False, statut=self.translate("Installation du mod"), max=max_step, step=0)
 
-        def replace_file(path, file, subpath="/"):
-            self.Progress(statut=self.translate("Modification de")+f"\n{get_nodir(path)}", add=1)
-            extension = get_extension(path)
+            def replace_file(path, file, subpath="/"):
+                self.Progress(statut=self.translate("Modification de")+f"\n{get_nodir(path)}", add=1)
+                extension = get_extension(path)
 
-            if extension == "szs":
-                if not (os.path.realpath(path) in extracted_file):
-                    subprocess.call(["./tools/szs/wszst", "EXTRACT", path, "-d", path + ".d", "--overwrite"]
-                                    , creationflags=CREATE_NO_WINDOW)
-                    extracted_file.append(os.path.realpath(path))
+                if extension == "szs":
+                    if not (os.path.realpath(path) in extracted_file):
+                        subprocess.call(["./tools/szs/wszst", "EXTRACT", path, "-d", path + ".d", "--overwrite"]
+                                        , creationflags=CREATE_NO_WINDOW)
+                        extracted_file.append(os.path.realpath(path))
 
-                szs_extract_path = path + ".d"
-                if os.path.exists(szs_extract_path + subpath):
-                    if subpath[-1] == "/":
-                        filecopy(f"./file/{file}", szs_extract_path + subpath + file)
-                    else:
-                        filecopy(f"./file/{file}", szs_extract_path + subpath)
+                    szs_extract_path = path + ".d"
+                    if os.path.exists(szs_extract_path + subpath):
+                        if subpath[-1] == "/":
+                            filecopy(f"./file/{file}", szs_extract_path + subpath + file)
+                        else:
+                            filecopy(f"./file/{file}", szs_extract_path + subpath)
 
-            elif path[-1] == "/":
-                filecopy(f"./file/{file}", path + file)
-            else:
-                filecopy(f"./file/{file}", path)
+                elif path[-1] == "/":
+                    filecopy(f"./file/{file}", path + file)
+                else:
+                    filecopy(f"./file/{file}", path)
 
-        for fp in fs:
-            for f in glob.glob(self.path_mkwf + "/files/" + fp, recursive=True):
-                if type(fs[fp]) == str:
-                    replace_file(path=f, file=fs[fp])
-                elif type(fs[fp]) == dict:
-                    for nf in fs[fp]:
-                        if type(fs[fp][nf]) == str:
-                            replace_file(path=f, subpath=nf, file=fs[fp][nf])
-                        elif type(fs[fp][nf]) == list:
-                            for ffp in fs[fp][nf]: replace_file(path=f, subpath=nf, file=ffp)
+            for fp in fs:
+                for f in glob.glob(self.path_mkwf + "/files/" + fp, recursive=True):
+                    if type(fs[fp]) == str:
+                        replace_file(path=f, file=fs[fp])
+                    elif type(fs[fp]) == dict:
+                        for nf in fs[fp]:
+                            if type(fs[fp][nf]) == str:
+                                replace_file(path=f, subpath=nf, file=fs[fp][nf])
+                            elif type(fs[fp][nf]) == list:
+                                for ffp in fs[fp][nf]: replace_file(path=f, subpath=nf, file=ffp)
 
-        for file in extracted_file:
-            self.Progress(statut=self.translate("Recompilation de")+f"\n{get_nodir(file)}", add=1)
-            subprocess.call(["./tools/szs/wszst", "CREATE", file + ".d", "-d", file,
-                             "--overwrite"], creationflags=CREATE_NO_WINDOW)
-            if os.path.exists(file + ".d"): shutil.rmtree(file + ".d")
+            for file in extracted_file:
+                self.Progress(statut=self.translate("Recompilation de")+f"\n{get_nodir(file)}", add=1)
+                subprocess.call(["./tools/szs/wszst", "CREATE", file + ".d", "-d", file,
+                                 "--overwrite"], creationflags=CREATE_NO_WINDOW)
+                if os.path.exists(file + ".d"): shutil.rmtree(file + ".d")
 
-        self.Progress(statut=self.translate("Patch main.dol"), add=1)
-        subprocess.call(["./tools/szs/wstrt", "patch", self.path_mkwf + "/sys/main.dol", "--clean-dol",
-                         "--add-lecode"], creationflags=CREATE_NO_WINDOW)
+            self.Progress(statut=self.translate("Patch main.dol"), add=1)
+            subprocess.call(["./tools/szs/wstrt", "patch", self.path_mkwf + "/sys/main.dol", "--clean-dol",
+                             "--add-lecode"], creationflags=CREATE_NO_WINDOW)
 
-        self.Progress(statut=self.translate("Patch lecode-PAL.bin"), add=1)
+            self.Progress(statut=self.translate("Patch lecode-PAL.bin"), add=1)
 
-        subprocess.call(
-            ["./tools/szs/wlect", "patch", "./file/lecode-PAL.bin", "-od", self.path_mkwf + "/files/rel/lecode-PAL.bin",
-             "--track-dir", self.path_mkwf + "/files/Race/Course/", "--copy-tracks", "./file/Track/",
-             "--move-tracks", self.path_mkwf + "/files/Race/Course/", "--le-define",
-             "./file/CTFILE.txt", "--lpar", "./file/lpar-default.txt", "--overwrite"], creationflags=CREATE_NO_WINDOW)
+            subprocess.call(
+                ["./tools/szs/wlect", "patch", "./file/lecode-PAL.bin", "-od", self.path_mkwf +
+                 "/files/rel/lecode-PAL.bin", "--track-dir", self.path_mkwf + "/files/Race/Course/", "--copy-tracks",
+                 "./file/Track/", "--move-tracks", self.path_mkwf + "/files/Race/Course/", "--le-define",
+                 "./file/CTFILE.txt", "--lpar", "./file/lpar-default.txt", "--overwrite"]
+                , creationflags=CREATE_NO_WINDOW)
 
-        outputformat = self.listbox_outputformat.get()
-        self.Progress(statut=self.translate("Conversion en")+f" {outputformat}", add=1)
+            outputformat = self.listbox_outputformat.get()
+            self.Progress(statut=self.translate("Conversion en")+f" {outputformat}", add=1)
 
-        if outputformat in ["ISO", "WBFS", "CISO"]:
-            self.path_mkwf_format = os.path.realpath(self.path_mkwf + "/../MKWFaraphel." + outputformat.lower())
-            subprocess.call(["./tools/wit/wit", "COPY", self.path_mkwf, "--DEST",
-                             self.path_mkwf_format, f"--{outputformat.lower()}", "--overwrite"]
-                            , creationflags=CREATE_NO_WINDOW)
-            shutil.rmtree(self.path_mkwf)
+            if outputformat in ["ISO", "WBFS", "CISO"]:
+                self.path_mkwf_format = os.path.realpath(self.path_mkwf + "/../MKWFaraphel." + outputformat.lower())
+                subprocess.call(["./tools/wit/wit", "COPY", self.path_mkwf, "--DEST",
+                                 self.path_mkwf_format, f"--{outputformat.lower()}", "--overwrite"]
+                                , creationflags=CREATE_NO_WINDOW)
+                shutil.rmtree(self.path_mkwf)
 
-            self.Progress(statut=self.translate("Changement de l'ID du jeu"), add=1)
-            subprocess.call(["./tools/wit/wit", "EDIT", self.path_mkwf_format, "--id", "RMCP60"]
-                            , creationflags=CREATE_NO_WINDOW)
+                self.Progress(statut=self.translate("Changement de l'ID du jeu"), add=1)
+                subprocess.call(["./tools/wit/wit", "EDIT", self.path_mkwf_format, "--id", "RMCP60"]
+                                , creationflags=CREATE_NO_WINDOW)
 
-        self.Progress(show=False)
-        messagebox.showinfo(self.translate("Fin"), self.translate("L'installation est terminé !"))
+            self.Progress(show=False)
+            messagebox.showinfo(self.translate("Fin"), self.translate("L'installation est terminé !"))
+
+        except:
+            self.log_error()
 
     t = Thread(target=func)
     t.setDaemon(True)
