8fdce2d4f5308445307b3e357a4c041884e17eb9
raphael60650 <rc60650@hotmail.com>
2021-06-15 17:25:47 +0200

    maked error.log relative

--- source/log_error.py
+++ source/log_error.py
@@ -1,4 +1,4 @@
 import traceback
 
 def log_error(self, exception):
-    with open("error.log", "a") as f: f.write(f"---\n{traceback.format_exc()}\n")
+    with open("./error.log", "a") as f: f.write(f"---\n{traceback.format_exc()}\n")
